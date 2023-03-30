e6a5e845d38ed18021b537cd1ba34661bbfc33a5
ibrahim6755 <ibrahimghani020@gmail.com>
2023-03-30 10:15:03 +0100

    navbar completed

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import './heroStyles.css'
 import heroimg from '../../imgs/mainhero.jpg'
+import heroimg2 from '../../imgs/mainhero2.jpg'
+import Navbar from '../navbar/Navbar'
 
 function Hero() {
   return (
     <>
-    <div className="hero-wrapper">
-      <img src={heroimg} alt="" />
-    </div>
-    <div className="overlay"></div>
+      
+
+      <div id="carouselExampleCaptions" className="carousel slide">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={heroimg} className="d-block w-100" alt="..." />
+            <div className="overlay"><Navbar/></div>
+            <div className="carousel-caption d-none d-md-block">
+              <h5 className='mb-5'>The latest product from the ecoshop</h5>
+              <h2 className='text-uppercase'>Look beautiful</h2>
+              <h1 className='text-uppercase fw-bold'>this season</h1>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={heroimg2} className="d-block w-100" alt="..." />
+            <div className="overlay"></div>
+            <div className="carousel-caption d-none d-md-block">
+              <h1>second slide label</h1>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './navStyles.css' 
+import mainlogo from '../../imgs/mainlogo.png'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+   <div className="container">
+     <nav className='mt-5'>
+    <ul>
+        <li><a href="#"><img src={mainlogo} alt="" /></a></li>
+        <li><a href="#" className='text-uppercase fs-5'>Products</a></li>
+        <li><a href="#" className='text-uppercase fs-5'>Store</a></li>
+        <li><a href="#" className='text-uppercase'>Designer</a></li>
+        <li><a href="#" className='text-uppercase'>Contact</a></li>
+    </ul>
+    <a href="#" className="btn mx-2 fs-5">Login</a>
+    <a href="#" className="btn mx-2 fs-5">Sign Up</a>
+</nav>
+   </div>
   )
 }
 
